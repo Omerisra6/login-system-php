595123e54a4c004bf341b056f45fe35320450700
omer <omer.israeli@boyar.org.il>
2022-02-28 21:10:45 +0200

    New: Api js file created

---
+++ client/api.js
@@ -0,0 +1,59 @@
+const getLoggedUsers =  async () => {
+
+    await fetch( '/server/get_logged_users.php', {
+        method: 'GET'
+    })
+    .then( ( res ) => res.json())
+    .then( async ( data ) => {
+
+        renderLoggedUsers( data )
+    })
+    .catch( ( error ) => {
+        
+        //On error ( user not logged ) redirect to login page
+        alert( 'Error: You are not logged in, please login again' )
+        window.location = '/client/forms/login.html' 
+        
+
+    })
+}
+
+const getUser = async ( id ) => {
+
+    let userDetails
+
+    await fetch( `/server/get_user.php?id=${ id }`, {
+        method: 'GET'
+    })
+    .then( ( res ) => res.json())
+    .then( data => {
+       userDetails = data
+    })
+    .catch( ( ) => {
+        
+        //On error ( user not logged ) redirect to login page
+        alert( 'Error: You are not logged in, please login again' )
+        window.location = '/client/forms/login.html' 
+        
+    })
+
+    return userDetails
+}
+
+const logOutUser =  async () => {
+   
+    await fetch( '/server/logout_user.php', {
+        method: 'GET'
+    }).then( () => {
+
+        //Redirecting to login page
+        window.location = '/client/forms/login.html'
+        
+    })
+}
+
+export{
+    getLoggedUsers,
+    getUser,
+    logOutUser
+}
