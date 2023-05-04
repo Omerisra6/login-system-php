a0d62e8db5a3d97ed3c3dd62a7bb2d0507415633
Omerisra06 <Omerisraeli6@gmail.com>
2023-05-04 13:40:18 +0300

    Tweak: Index script updated

--- public/scripts/pages/index.js
+++ public/scripts/pages/index.js
@@ -1,92 +1,53 @@
-import { getLoggedUsers, logOutUser, getUser  } from "../utils/api.js"
-import { _ } from "../utils/helpers.js"
+import { getLoggedUsers, logOutUser  } from "../utils/api.js"
+import { _  } from "../utils/helpers.js"
+import { UserRow } from "../view-components/UserRow.js"
 const loggedUsersTable            = _( '.logged-users-table-body' )
 const logOutButton                = _( '.logout-button' )
 const loggedUserDetailsPopWrapper = _( '.user-details-pop-wrapper' )  
-const loggedUserDetailsContainer  = _( '.logged-user-details-container' )
-
-logOutButton.addEventListener( 'click', async () => {
-    await logOutUser()
-})
 
 window.addEventListener( 'load', async () => {
-    renderLoggedUsers( await getLoggedUsers() )
+
+    renderLoggedUsers()
 } )
 
 window.setInterval( async () => {
-    renderLoggedUsers( await getLoggedUsers() )  
+
+    renderLoggedUsers()  
 }, 3000);
+
+
+logOutButton.addEventListener( 'click', async () => {
+
+    logOutButton.disabled = true
+
+    try {
+        await logOutUser()
+        window.location.href = '/login'
+    } catch ( error ) {
+        alert( 'Something went wrong, please try refreshing the page')
+        logOutButton.disabled = false
+    }
+})
 
 
 loggedUserDetailsPopWrapper.addEventListener( 'click', () => {
 
     loggedUserDetailsPopWrapper.classList.toggle( 'invisible' )
-
 })
 
-function renderLoggedUsers( users ){
+async function renderLoggedUsers(  ){
+
+    let users
+    try {
+        users = await getLoggedUsers()
+    } catch ( error ) {
+        alert( 'Something went wrong, please try refreshing the page')
+    }
 
     loggedUsersTable.innerHTML = ''
 
     users.forEach( user => {
-        renderLoggedUser( user )
+
+        loggedUsersTable.appendChild( UserRow( user ) )
     });
-
-    attachListenersToUsers( )
-
-}
-
-function renderLoggedUser( user ){
-
-    const date = ( new Date( user[ 'last_action' ] * 1000 ) ).toLocaleString().replaceAll( '/', '-' )
-
-    loggedUsersTable.innerHTML += `
-        <tr class="logged-user" data-id=${ user[ 'id' ] }>
-            <td> ${ user[ 'username' ] } </td>
-            <td> ${ user[ 'last_login' ] } </td>
-            <td> ${ date  } </td>
-            <td> ${ user[ 'ip' ] } </td>
-        </tr>
-    `
-}
-
-const renderUserToPop = ( userDetails ) => {
-
-    loggedUserDetailsContainer.innerHTML = 
-    `
-        <h2 class="pop-title"> ${ userDetails[ 'username' ] }'s details </h2>
-
-        <div class="user-detail">
-            <h5> User Agent </h5>
-            <h3> ${ userDetails[ 'user_agent' ] } </h3>
-        </div>
-
-        <div class="user-detail">
-            <h5> Register Time</h5>
-            <h3> ${ userDetails[ 'register_time' ] } </h3>
-        </div>
-
-        <div class="user-detail">
-            <h5> Login count </h5>
-            <h3> ${ userDetails[ 'login_count' ] } </h3>
-        </div>
-        
-    `
-}
-
-async function attachListenersToUsers(){
-
-    const loggedUsers = document.querySelectorAll( '.logged-user' )
-
-    loggedUsers.forEach( user => {
-
-        user.addEventListener( 'click', async () => {
-            const id    = user.dataset.id
-            const userDetails = await getUser( id )
-            renderUserToPop( userDetails )
-            loggedUserDetailsPopWrapper.classList.toggle( 'invisible' )
-        })    
- 
-    });
-
 }
