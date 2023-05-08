bf6de79957a9fbfe51dd637b5d7a0d437f0ceaa9
Omerisra06 <Omerisraeli6@gmail.com>
2023-05-08 16:31:19 +0300

    Fix: Index paths fixed

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 session_start();
 
 define( 'PUBLIC_PATH', __DIR__ . './public' );
-define( 'DB_DIR', __DIR__ . 'App/DB' );
+define( 'DB_DIR', __DIR__ . 'app/DB' );
 
-require_once( __DIR__ . 'App/autoload.php' );
+require_once( __DIR__ . 'app/autoload.php' );
 
-require_once( __DIR__ . 'App/routes.php' );
+require_once( __DIR__ . 'app/routes.php' );
