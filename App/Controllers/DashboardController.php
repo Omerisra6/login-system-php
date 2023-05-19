332ee9769f925458dac1932153804eec9c027a9d
Omerisra06 <Omerisraeli6@gmail.com>
2023-05-19 13:29:15 +0300

    Tweak: Unnecessary headers removed

--- App/Controllers/DashboardController.php
+++ App/Controllers/DashboardController.php
@@ -5,12 +5,6 @@ namespace App\Controllers;
 use App\Utils\HtmlResponse;
 use App\Utils\Response;
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
 class DashboardController
 {
     public function show()
